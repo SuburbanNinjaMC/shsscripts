423601b74ac7fb2cafc605bb1a8f6dc309476512
SuburbanNinjaMC <suburbanninja575@gmail.com>
2022-09-01 21:16:46 -0400

    Update permutationsofstring.java

--- permutationsofstring.java
+++ permutationsofstring.java
@@ -1,33 +1,46 @@
-public class MyClass {
+public class Permutations {
+    // Discovered this problem online searching for "difficult recursion practice." Was a fun challenge.
+    
     public static void permutations(String str) {
         permutations(str.toCharArray(), 0, 0);
     }
     
-    public static void permutations(char[] list, int pivot, int start) {
-        if (pivot == list.length) {
+    public static void permutations(char[] list, int index, int start) {
+        // Once the index has reached the end of the array, we're done.
+        if (index == list.length - 1) {
             System.out.println(charArray(list, 0, list.length));
             return;
         }
-        
-        if (start == list.length) {
-            return;
+
+        // Repeated swaps that allow us to randomize the array. We keep the first letter constant and then recursively permute the rest with the same logic.
+        // Attempted to make this completely recursive. Failed. Hybrid solution implemented instead.
+        start = index;
+        while (start < list.length) {
+            swap(list, index, start);
+            // Recursive call.
+            permutations(list, index + 1, start);
+            
+            // When we swap it back, we wind up on the same letter.
+            swap(list, index, start);
+            start++;
         }
-        
-        swap(list, pivot, start);
-        permutations(list, pivot + 1, start);
-        swap(list, pivot, start);
-        
-        permutations(list, pivot, start + 1);
     }
-    
+
+    //  While the String.valueOf(char[] T) exists, I decided to go full recursive here.
     public static String charArray(char[] vals, int start, int end) {
         if (start == end) return "";
         return vals[start] + charArray(vals, start + 1, end);
     }
     
+    // Basic swap method, learned from my old computer science course.
     public static void swap(char[] arr, int aIndex, int bIndex) {
         char temp = arr[aIndex];
         arr[aIndex] = arr[bIndex];
         arr[bIndex] = temp;
+    }
+    
+    // Tester method.
+    public static void main(String[] args) {
+        permutations("abcd");
     }
 }
